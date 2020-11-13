3edaa9b89166c8042cd2e14ae20df038e7e64e68
eukurapov <eukurapov@gmail.com>
2020-11-13 15:24:21 +0300

    Supported scrolling in details view

--- Movies/Movies/DetailsViewController.swift
+++ Movies/Movies/DetailsViewController.swift
@@ -17,7 +17,7 @@ class DetailsViewController: UIViewController {
             if let movie = movie {
                 activityIndicatior.startAnimating()
                 nameLabel.text = movie.name
-                descriptionLabel.text = movie.description
+                descriptionLabel.text = movie.description + " " + movie.description
                 updateRatingMaskLayer()
                 MovieService.shared.fetchImageFrom(path: movie.imagePath) { [weak self] result in
                     self?.activityIndicatior.stopAnimating()
@@ -33,7 +33,7 @@ class DetailsViewController: UIViewController {
         }
     }
     
-    private var wrapper = UIView()
+    private var wrapper = UIScrollView()
     lazy var imageView = UIImageView()
     private lazy var nameLabel = UILabel.withTextStyle(.headline)
     private lazy var ratingLabel = UILabel.withTextStyle(.subheadline)
@@ -45,6 +45,7 @@ class DetailsViewController: UIViewController {
         super.viewDidLoad()
         
         let panGestureRecogniser = UIPanGestureRecognizer(target: self, action: #selector(panGesture))
+        panGestureRecogniser.delegate = self
         view.addGestureRecognizer(panGestureRecogniser)
         
         style()
@@ -55,6 +56,8 @@ class DetailsViewController: UIViewController {
         modalPresentationCapturesStatusBarAppearance = true
         wrapper.layer.masksToBounds = true
         wrapper.backgroundColor = .movieDarkPurple
+        wrapper.contentInsetAdjustmentBehavior = .never
+        wrapper.showsVerticalScrollIndicator = false
         view.backgroundColor = .clear
         imageView.contentMode = .scaleAspectFill
         nameLabel.textColor = .movieText
@@ -78,30 +81,32 @@ class DetailsViewController: UIViewController {
         ratingLabel.translatesAutoresizingMaskIntoConstraints = false
         activityIndicatior.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
+            wrapper.topAnchor.constraint(equalTo: view.topAnchor),
+            wrapper.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: wrapper.trailingAnchor),
+            wrapper.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            
             imageView.topAnchor.constraint(equalTo: wrapper.topAnchor),
-            imageView.leadingAnchor.constraint(equalTo: wrapper.leadingAnchor),
-            wrapper.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 9/16, constant: 0),
             
             nameLabel.topAnchor.constraint(equalToSystemSpacingBelow: imageView.bottomAnchor, multiplier: 1),
-            nameLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: wrapper.leadingAnchor, multiplier: 1),
-            wrapper.trailingAnchor.constraint(equalToSystemSpacingAfter: nameLabel.trailingAnchor, multiplier: 1),
+            nameLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: nameLabel.trailingAnchor, multiplier: 1),
             
             ratingLabel.topAnchor.constraint(equalToSystemSpacingBelow: nameLabel.bottomAnchor, multiplier: 1),
-            ratingLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: wrapper.leadingAnchor, multiplier: 1),
-            wrapper.trailingAnchor.constraint(equalToSystemSpacingAfter: ratingLabel.trailingAnchor, multiplier: 1),
+            ratingLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: ratingLabel.trailingAnchor, multiplier: 1),
             
             descriptionLabel.topAnchor.constraint(equalToSystemSpacingBelow: ratingLabel.bottomAnchor, multiplier: 1),
-            descriptionLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: wrapper.leadingAnchor, multiplier: 1),
-            wrapper.trailingAnchor.constraint(equalToSystemSpacingAfter: descriptionLabel.trailingAnchor, multiplier: 1),
+            descriptionLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: descriptionLabel.trailingAnchor, multiplier: 1),
+            wrapper.bottomAnchor.constraint(equalTo: descriptionLabel.bottomAnchor),
             
             activityIndicatior.centerXAnchor.constraint(equalTo: imageView.centerXAnchor),
             activityIndicatior.centerYAnchor.constraint(equalTo: imageView.centerYAnchor),
             
-            wrapper.topAnchor.constraint(equalTo: view.topAnchor),
-            wrapper.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            view.trailingAnchor.constraint(equalTo: wrapper.trailingAnchor),
-            view.bottomAnchor.constraint(equalTo: wrapper.bottomAnchor),
         ])
     }
     
@@ -117,8 +122,7 @@ class DetailsViewController: UIViewController {
     
     @objc
     private func panGesture(_ sender: UIPanGestureRecognizer) {
-        let dY = sender.translation(in: wrapper).y
-        
+        let dY = sender.translation(in: wrapper).y - wrapper.contentOffset.y
         guard dY > 0 else { return }
         
         let scale = 1 - (dY / wrapper.bounds.height)
@@ -141,6 +145,14 @@ class DetailsViewController: UIViewController {
             }
         default: break
         }
+    }
+    
+}
+
+extension DetailsViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
     
 }
